65d450276fb2fd59b97e14bb110373199f355175
@manat <45030304+sommanat@users.noreply.github.com>
2025-04-25 10:35:54 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "sexy girl with sword",
     };
 
     const response = await env.AI.run(
