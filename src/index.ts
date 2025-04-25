f3fababb8473ed4b8537f7b1932373d0da336035
@manat <45030304+sommanat@users.noreply.github.com>
2025-04-25 10:53:22 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export default {
 
     const response = await env.AI.run(
       //"@cf/stabilityai/stable-diffusion-xl-base-1.0",
-      "@cf/bytedance/stable-diffusion-xl-lightning",
+      "@cf/black-forest-labs/flux-1-schnell",
       inputs,
     );
 
